d2f30352fa9ea37510b526b55be1bc8793b1a4d1
Aravind Pai <dragondive@outlook.in>
2023-07-29 21:57:28 +0530

    feat: add helloworld.py

---
+++ src/helloworld.py
@@ -0,0 +1,10 @@
+"""Hello World module"""
+
+class HelloWorld:
+    """Hello World class"""
+    def __init__():
+        pass
+
+    def get_hello_world():
+        """Get message Hello World."""
+        return "Hello World"
