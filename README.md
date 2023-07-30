3f6ad2784f75285f19481492b838eeb86d71dd2a
Aravind Pai <dragondive@outlook.in>
2023-07-30 17:41:55 +0530

    feat: setup portray documentation generator

---
+++ README.md
@@ -0,0 +1,4 @@
+Contract Consumer
+==================
+
+This is the consumer component of my project to learn, prepare and demo contract testing.
---
+++ src/contract_consumer/__main__.py
@@ -0,0 +1,9 @@
+"""Perform starting configurations for the contract consumer."""
+
+
+def entry_point():
+    """Entry point function that sets up the contract consumer project."""
+    # nothing to do here for now.
+
+
+entry_point()
