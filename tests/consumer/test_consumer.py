cb42e5cdbdd944ad6e814c7220df9ba2f4fdd923
Aravind Pai <dragondive@outlook.in>
2023-07-31 09:47:22 +0530

    test: rework unit test to use consumer fixture

--- tests/consumer/test_consumer.py
+++ tests/consumer/test_consumer.py
@@ -4,19 +4,24 @@ import pytest
 from consumer.consumer import Consumer
 
 
+@pytest.fixture
+def consumer():
+    return Consumer(base_uri="https://jsonplaceholder.typicode.com")
+
 @pytest.mark.parametrize(
     "userid",
     range(1, 10),
 )
-def test_get_user_info_with_valid_userid_returns_valid_user_info(userid):
+def test_get_user_info_with_valid_userid_returns_valid_user_info(consumer, userid):
     """Check the user info fetched for the userids.
 
     Parameters
     ----------
+    consumer : consumer.consumer.Consumer
+        Fixture for the consumer that connects to the pact mock server.
     userid : int
         userid for which information is to be fetched.
     """
-    consumer = Consumer(base_uri="https://jsonplaceholder.typicode.com")
     user_info = consumer.get_user_info(userid=userid)
     assert user_info is not None
     assert isinstance(user_info.name, str)
